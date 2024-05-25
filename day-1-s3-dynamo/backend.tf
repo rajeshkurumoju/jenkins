a60b49af46c6d76178fb88979a188151183ec91d
rajesh <kurumojurajeshkumar535@gmail.com>
2024-05-25 20:38:56 +0530

    fff

---
+++ day-1-s3-dynamo/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "rajesh-raj"
+    key = "terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "terraform-state-lock-dynamo"
+    encrypt = true
+
+   
+}
+}
