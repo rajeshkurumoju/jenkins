bbc77119c7d10ba8279cbf71fa989f64f3ff6480
rajesh <kurumojurajeshkumar535@gmail.com>
2024-05-26 16:17:25 +0530

    ff

--- day-1-s3-dynamo/backend.tf
+++ day-1-s3-dynamo/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "rajesh-yyy"
+    bucket = "ghchgcfxdgxfgcf"
     key = "terraform.tfstate"
     region = "ap-south-1"
     dynamodb_table = "terraform-state-lock-dynamo"
--- day-1-s3-dynamo/main.tf
+++ day-1-s3-dynamo/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "raj" {
-    bucket = "rajesh-yyy"
+    bucket = "ghchgcfxdgxfgcf"
   
 }
 
