fea1a0114cb876a363e664b77da16c480713d8be
rajesh <kurumojurajeshkumar535@gmail.com>
2024-05-27 13:39:57 +0530

    gfg

---
+++ sample-ec2/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+region = "ap-south-1"
+}
+
+resource "aws_instance" "raj" {
+  ami = "ami-013e83f579886baeb"
+  instance_type = "t2.micro"
+  key_name = "sss"
+  tags = {
+    Name = "luv"
+  }
+}
